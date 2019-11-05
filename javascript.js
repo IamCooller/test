114b763db858f69d3456f7666c18ef8b04e3d06c
cooller2000@inbox.ru <cooller2000@inbox.ru>
2019-11-06 01:28:11 +0300

    Это мой первый комит

---
+++ javascript.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(1);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol('d');
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+let persone = {
+    name: "John",
+    age:25,
+    isMarried:false
+}
+
+console.log(persone.name);
+
+
+
+//alert("Hello Man");
+
+// let answer = prompt("You ok", "Ye");
+
+// console.log(answer);
+
